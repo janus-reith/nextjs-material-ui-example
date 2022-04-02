366db2f08a8982880d0c9bdf98841ee31783a1af
Janus Reith <mail@janusreith.de>
2022-04-02 13:16:36 +0200

    Don't prefetch

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ProTip from "../src/ProTip";
+import Link from "../src/Link";
+import Copyright from "../src/Copyright";
 
 export default function Index() {
   return (
@@ -13,7 +13,7 @@ export default function Index() {
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
+        <Link href="/about" color="secondary" prefetch={false}>
           Go to the about page
         </Link>
         <ProTip />
--- pages/onlyBox.js
+++ pages/onlyBox.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+
 export default function Index() {
   return (
     <Container maxWidth="sm">
