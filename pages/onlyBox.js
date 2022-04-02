654e7c354e1f02d3b1efcb4ad24698d5615588b4
Janus Reith <mail@janusreith.de>
2022-04-02 13:14:09 +0200

    Add content

--- pages/onlyBox.js
+++ pages/onlyBox.js
@@ -1,6 +1,15 @@
 import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-
-export default function BoxPage() {
-  return <Box sx={{ my: 4 }}>Just a MUI Box</Box>;
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js example
+        </Typography>
+      </Box>
+    </Container>
+  );
 }
