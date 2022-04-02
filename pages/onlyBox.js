7b9e004839a59fcdc7be48dd51e277ae9c0a62db
Janus Reith <mail@janusreith.de>
2022-04-02 13:10:31 +0200

    Add Box +2

--- pages/onlyBox.js
+++ pages/onlyBox.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 
-export default function Box() {
+export default function BoxPage() {
   return <Box sx={{ my: 4 }}>Just a MUI Box</Box>;
 }
